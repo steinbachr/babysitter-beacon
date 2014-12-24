ba69a50b774832985af488db13bf845e8a876d13
Bobby <steinbach.rj@gmail.com>
2014-12-24 16:25:41 -0500

    some minor tweaks to settings file

--- Babysitter/settings.py
+++ Babysitter/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -46,6 +45,7 @@ INSTALLED_APPS = (
     'widget_tweaks',
     'pipeline',
     'rest_framework',
+    'Babysitter',
     'api',
     'web'
 )
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hellodjango.wsgi --log-file -
+web: gunicorn Babysitter.wsgi --log-file -
